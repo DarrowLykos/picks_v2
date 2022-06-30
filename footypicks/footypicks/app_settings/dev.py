0506523c0b6475e99250d57cf780af099de8ebd7
Sam Jones <61124791+DarrowLykos@users.noreply.github.com>
2022-06-30 18:02:11 +0100

    Revert "Delete dev.py"
    This reverts commit a7a6242040b9c5bf89edec262a06c3ee67fac59b.

---
+++ footypicks/footypicks/app_settings/dev.py
@@ -0,0 +1,22 @@
+from .base_settings import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!+)p=ft^jr+$kni!c1w*9!xaj)*6)ir)4zh!(=b=8a8j!xf#b-'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'footypicks.com', '192.168.1.163', 'sjones187.ddns.net', ]
+
+# Email backend
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent-emails')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
